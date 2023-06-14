d15b2a92e776f61e91a14e8a37f52e40b8eef485
jdbpt <jbrown5.filn@gmail.com>
2023-06-14 12:15:26 -0400

    fourth commit

--- wk4_day1_lab/script.js
+++ wk4_day1_lab/script.js
@@ -11,7 +11,6 @@ minusOne(100);
 minusOne(Infinity);
 
 //makeSentence function
-
 const makeSentence = function(str0, str1, str2){
     //console.log(`Once upon a time, there was a person named ${str0} who loved ${str1} and just wanted to see the ${str2}`)
     console.log("Once upon a time, there was a person named " + str0 + " who loved " + str1 + " and just wanted to see the " + str2);
@@ -21,7 +20,6 @@ const makeSentence = function(str0, str1, str2){
 makeSentence("Herbert", "seaweed", "sea");
 
 //getLastElement function
-
 const getLastElement = function(arr){
     console.log(arr[arr.length-1]);
 
