ea174d65b75ebaeb303dd29ddf0cf6f2034f08a0
jdbpt <jbrown5.filn@gmail.com>
2023-06-19 21:45:58 -0400

    created person class

--- wk4_day1_hw/script.js
+++ wk4_day1_hw/script.js
@@ -1,4 +1,4 @@
-/**Week 4 Day 1 HW
+/*Week 4 Day 1 HW
  * Hardmode is not required
  * for each section
  * at least 2-3 methods for each one
@@ -6,10 +6,10 @@
  * Complete all else including Chef
  */
 
-/**JavaScript Classes Lab 3 */
+/*JavaScript Classes Lab 3 */
 
 //Hamster
-
+/**A Hamster object, with owner, name, price, and methods*/
 class Hamster{
     constructor(name){
         this.owner = "";
@@ -28,4 +28,68 @@ class Hamster{
     getPrice(){
         return (this.price);
     }
+}//end Hamster class
+
+
+//testing*********Hamster Class
+const hammy = new Hamster("Hammy");
+hammy.wheelRun(), hammy.eatFood(), console.log(hammy.getPrice());
+class Person{
+    constructor(name){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;//integer
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getAge(){
+        return this.age;
+    }
+
+    getWeight(){
+        return this.weight;
+    }
+
+    greet(){
+        console.log(`${this.name} says \"Hello!\"`)
+    }
+
+    eat(){
+        //improve mood, and increase weight
+        this.mood += 5;
+        this.weight += 2;
+    }
+
+    exercise(){
+        //decrease weight
+        this.weight -= 0.12;
+    }
+
+    ageUp(){
+        //decrement mood, all else increment
+        this.age += 1;
+        this.height += 0.25;//feet
+        this.weight += 2;
+        this.mood -= 1;
+        this.bankAccount += 10;
+
+    }
+
+    buyHamster(hamster){
+        let newHamster = new Hamster(hamster);
+        this.hamsters.push(newHamster);
+        //increment mood, decrement bankAccount
+        this.mood += 10;
+        this.bankAccount -= newHamster.getPrice();
+    }
+
+
 }
+
